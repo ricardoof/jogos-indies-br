8ad28c938883ac0901818fb4d34d07736bab1ade
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-09-07 12:01:07 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,26 @@
+let jogos = [
+    {
+        nome: "Enigma do Medo",
+        estudio: "Dumativa",
+        descricao: "Enigma do Medo é um jogo de Investigação e Suspense em Pixel Art/3D onde você será Mia, uma jovem detetive que investiga o paranormal em uma misteriosa área denominada Perímetro - O lugar que não existe.",
+        link: "https://store.steampowered.com/app/1507580/Enigma_do_Medo/"
+    },
+    {
+        nome: "Fobia",
+        estudio: "Pulsatrix Studios",
+        descricao: "Treze Trilhas é lar do Hotel Santa Dinfna, palco de diversos boatos, incluindo sumiços misteriosos e atividade paranormal. Em busca de um furo, o jornalista Roberto Leite Lopes viaja até Santa Catarina atrás de uma dica de Stephanie. Suas habilidades investigativas o ajudarão a descobrir a verdade e a sobreviver quando encontra uma câmera que revela outras linhas do tempo, uma seita, experimentos com humanos e aparições. Resolva puzzles e procure algo para detê-las, enquanto passado, presente e futuro colidem.",
+        link: "https://store.steampowered.com/app/1298140/Fobia__St_Dinfna_Hotel/"
+    },
+    {
+        nome: "Mark of the Deep",
+        estudio: "Mad Mimic",
+        descricao: "Mark of the Deep é uma épica aventura pirata com uma narrativa forte e uma empolgante mistura de elementos de jogos Metroidvania e Souls-Like. Embarque nesta jornada como o pirata Rookie, explore os mistérios de uma ilha amaldiçoada e encontre sua tripulação desaparecida!",
+        link: "https://store.steampowered.com/app/1933370/Mark_of_the_Deep/"
+    },
+    {
+        nome: "Dandy Ace",
+        estudio: "Mad Mimic",
+        descricao: "Dandy Ace é um roguelike com uma experiência de combate dinâmico e extravagante que segue um mágico fabuloso buscando combinar e usar suas cartas mágicas enquanto luta e saqueia para derrotar o Ilusionista de olhos verdes, Lele, que o aprisionou em um espelho amaldiçoado. Enquanto batalha pelo palácio que sempre muda de Lele, combine diferentes cartas com mais de mil possibilidades, cada uma com seus próprios estilos de jogo e poderes. Cada partida oferece novos desafios e combinações para os jogadores explorarem à medida que chegam mais perto de Lele.",
+        link: "https://store.steampowered.com/app/1037130/Dandy_Ace/"
+    }
+];
