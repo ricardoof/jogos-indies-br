5d677a0104172b2d50c79c66af0ac91a83f94380
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-09-07 14:04:59 -0300

    adição jogos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(jogos);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = ""
+
+    for (let jogo of jogos) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>${jogo.nome}</h2>
+                <p class="descricao-meta">${jogo.descricao}</p>
+                <a href=${jogo.link} target="_blank">Página na Steam</a>
+            </div>
+        `
+    }
+
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -3,24 +3,56 @@ let jogos = [
         nome: "Enigma do Medo",
         estudio: "Dumativa",
         descricao: "Enigma do Medo é um jogo de Investigação e Suspense em Pixel Art/3D onde você será Mia, uma jovem detetive que investiga o paranormal em uma misteriosa área denominada Perímetro - O lugar que não existe.",
+        imagem: "imagens/enigma-do-medo.jpg",
         link: "https://store.steampowered.com/app/1507580/Enigma_do_Medo/"
     },
     {
-        nome: "Fobia",
+        nome: "Fobia - St. Dinfna Hotel",
         estudio: "Pulsatrix Studios",
         descricao: "Treze Trilhas é lar do Hotel Santa Dinfna, palco de diversos boatos, incluindo sumiços misteriosos e atividade paranormal. Em busca de um furo, o jornalista Roberto Leite Lopes viaja até Santa Catarina atrás de uma dica de Stephanie. Suas habilidades investigativas o ajudarão a descobrir a verdade e a sobreviver quando encontra uma câmera que revela outras linhas do tempo, uma seita, experimentos com humanos e aparições. Resolva puzzles e procure algo para detê-las, enquanto passado, presente e futuro colidem.",
+        imagem: "imagens/fobia.jpg",
         link: "https://store.steampowered.com/app/1298140/Fobia__St_Dinfna_Hotel/"
     },
     {
         nome: "Mark of the Deep",
         estudio: "Mad Mimic",
         descricao: "Mark of the Deep é uma épica aventura pirata com uma narrativa forte e uma empolgante mistura de elementos de jogos Metroidvania e Souls-Like. Embarque nesta jornada como o pirata Rookie, explore os mistérios de uma ilha amaldiçoada e encontre sua tripulação desaparecida!",
+        imagem: "imagens/mark-of-the-deep.jpg",
         link: "https://store.steampowered.com/app/1933370/Mark_of_the_Deep/"
     },
     {
         nome: "Dandy Ace",
         estudio: "Mad Mimic",
         descricao: "Dandy Ace é um roguelike com uma experiência de combate dinâmico e extravagante que segue um mágico fabuloso buscando combinar e usar suas cartas mágicas enquanto luta e saqueia para derrotar o Ilusionista de olhos verdes, Lele, que o aprisionou em um espelho amaldiçoado. Enquanto batalha pelo palácio que sempre muda de Lele, combine diferentes cartas com mais de mil possibilidades, cada uma com seus próprios estilos de jogo e poderes. Cada partida oferece novos desafios e combinações para os jogadores explorarem à medida que chegam mais perto de Lele.",
+        imagem: "imagens/dandy.jpg",
         link: "https://store.steampowered.com/app/1037130/Dandy_Ace/"
+    },
+    {
+        nome: "A lenda do herói",
+        estudio: "Dumativa",
+        descricao: "Plataformas voadoras, inimigos coloridos, armas mágicas e física inexplicável. Isso tudo com uma música de fundo que acompanha a saga de um cavaleiro. Eis um breve resumo do que poderá ser encontrado em A Lenda do Herói - O Jogo, que homenageia vários clássicos das eras 8 e 16 bits, como The Legend of Zelda e Wonder Boy in Monster Land, e ao mesmo tempo tira sarro de diversos clichês encontrados no mundo dos games.",
+        imagem: "imagens/a-lenda-do-heroi.jpg",
+        link: "https://store.steampowered.com/app/389170/A_Lenda_do_Heroi__Edicao_Definitiva/"
+    },
+    {
+        nome: "MULLET MADJACK",
+        estudio: "HAMMER95",
+        descricao: "Você tem 10 SEGUNDOS de VIDA: Você precisa MATAR para ganhar mais SEGUNDOS: “ Em um futuro distante, HUMANIDADE e INTERNET se fundem em um novo ser. Um humano precisa de DOPAMINA a cada 10 SEGUNDOS ou ele MORRE!",
+        imagem: "imagens/mullet-madjack.jpg",
+        link: "https://store.steampowered.com/app/2111190/MULLET_MADJACK/"
+    },
+    {
+        nome: "Horizon Chase Turbo",
+        estudio: "AQUIRIS",
+        descricao: "Horizon Chase Turbo é um jogo de corrida inspirado nos grandes sucessos dos anos 80 e 90: Out Run, Lotus Turbo Challenge, Top Gear (SNES), Rush, entre outros. Cada curva e cada volta no Horizon Chase Turbo recria a jogabilidade de arcade clássica e oferece velocidade e diversão ilimitadas. Pise fundo e divirta-se!",
+        imagem: "imagens/horizon-chase.jpg",
+        link: "https://store.steampowered.com/app/2111190/MULLET_MADJACK/"
+    },
+    {
+        nome: "A.I.L.A",
+        estudio: "Pulsatrix Studios",
+        descricao: "Mergulhe em cenários de terror perturbadores como um testador de jogo. Sobreviva a experiências sombrias e inquietantes criadas por A.I.L.A. que se alimentam dos seus medos mais profundos. Esquive-se e escape de uma seita ritualística, solucione enigmas sangrentos, entre num combate visceral contra mortos-vivos medievais e muito mais.",
+        imagem: "imagens/aila.jpg",
+        link: "https://store.steampowered.com/app/2695430/AILA/"
     }
 ];
