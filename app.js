1b9e9613b3556f05050522b89b705f7f1c97df39
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2024-09-08 10:58:15 -0300

    adição busca

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    if(!campoPesquisa) {
+        section.innerHTML = "<p class=nao-encontrado>Nada foi encontrado. Você precisa digitar o nome de um jogo ou estudio</p>"
+        return;
+    }
+    campoPesquisa = campoPesquisa.toLowerCase();
 
-    let resultados = ""
+    let resultados = "";
+    let nome = "";
+    let estudio = "";
+    let tags = "";
+    let imagensPossiveis = [];
 
     for (let jogo of jogos) {
-        resultados += `
-            <div class="item-resultado">
-                <h2>${jogo.nome}</h2>
-                <p class="descricao-meta">${jogo.descricao}</p>
-                <a href=${jogo.link} target="_blank">Página na Steam</a>
-            </div>
-        `
+        nome = jogo.nome.toLowerCase();
+        estudio = jogo.estudio.toLowerCase();
+        tags = jogo.tags.toLocaleLowerCase();
+
+        if(nome.includes(campoPesquisa) || estudio.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <h2>${jogo.nome}</h2>
+                    <h3>${jogo.estudio}</h2>
+                    <p class="descricao-meta">${jogo.descricao}</p>
+                    <a href=${jogo.link} target="_blank">Página na Steam</a>
+                </div>
+            `;
+            imagensPossiveis.push(jogo.imagem);
+        }
+    }
+
+    if(!resultados) {
+        resultados = "<p class=nao-encontrado>Jogo não encontrado ou não cadastrado</p>"
     }
 
     section.innerHTML = resultados;
+    if (imagensPossiveis.length > 0) {
+        const indiceAleatorio = Math.floor(Math.random() * imagensPossiveis.length);
+        const imagemAleatoria = imagensPossiveis[indiceAleatorio];
+        document.body.style.backgroundImage = `url(${imagemAleatoria})`;
+    }
 }
